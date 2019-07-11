bb55244b875240cb6fd7523634a23a4cf3ec0f91
Gian Franco Zabarino <gfzabarino@gmail.com>
2019-07-11 00:54:15 -0300

    Fixed XSS bug.

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ class WebsiteMessage extends TextMessage {
     if (fs.existsSync(localPath)) {
       return appendMessage();
     }
-    webshot(link, localPath, { defaultWhiteBackground: true, windowSize: { width: 480, height: 320}, renderDelay: 1000 }, (err) => {
+    webshot(link, localPath, { defaultWhiteBackground: true, windowSize: { width: 480, height: 480}, renderDelay: 1000 }, (err) => {
       if (err) {
         console.error('Error while fetching ' + link + ': ' + err.stack);
         return fallbackMessage();
